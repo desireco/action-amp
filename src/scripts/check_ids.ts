f37dbe01b2b1bf7f0a02dfb46dc78dcf115a494b
Zeljko (Jake) Dakic <jake@augmented-devs.com>
2025-11-28 10:13:13 -0600

    Remove temporary script

--- src/scripts/check_ids.ts
+++
@@ -1,9 +0,0 @@
-
-import { getCollection } from 'astro:content';
-
-export async function printProjectIds() {
-    const projects = await getCollection('projects');
-    console.log('Project IDs:', projects.map(p => p.id));
-}
-
-printProjectIds();
